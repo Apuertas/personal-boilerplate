8550798be787e53daf603164be0e62db98ddbd93
Alex Puertas <alex.puertas96@gmail.com>
2017-11-05 11:50:09 +0100

    Add redux

--- src/client.jsx
+++ src/client.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers/counter';
 
 import './main.css';
-import App from './App/App';
+import App from './containers/App';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <Provider store={store}>
+        <Component />
+      </Provider>
     </AppContainer>,
     document.getElementById('root'),
   );
@@ -18,5 +28,5 @@ render(App);
 
 // Webpack Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept('./App/App', () => { render(App); });
+  module.hot.accept('./containers/App', () => { render(App); });
 }
